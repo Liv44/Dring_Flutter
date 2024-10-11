36a35c42d82df1c03f20ead4885cd7fd1313f209
LBROCHARD <73283488+LBROCHARD@users.noreply.github.com>
2024-10-11 16:10:12 +0200

    :sparkles: added selection of timer

--- lib/widgets/timeSelectionWidget.dart
+++ lib/widgets/timeSelectionWidget.dart
@@ -9,9 +9,11 @@ class TimeSelectionWidget extends StatefulWidget {
     super.key,
     this.focusDuration = 25,
     this.pauseDuration = 5,
+    this.isInUse = false,
   });
   final double focusDuration;
   final double pauseDuration;
+  final bool isInUse;
 
   @override
   State<TimeSelectionWidget> createState() => _TimeSelectionWidgetState();
@@ -21,8 +23,6 @@ class _TimeSelectionWidgetState extends State<TimeSelectionWidget> {
   @override
   Widget build(BuildContext context) {
     final provider = Provider.of<TimerService>(context);
-    bool isInUse = false;
-    
     return Container(
       width: 100, height: 40, 
       padding: const EdgeInsets.all(0),
@@ -36,13 +36,13 @@ class _TimeSelectionWidgetState extends State<TimeSelectionWidget> {
           overlayColor: WidgetStatePropertyAll(Colors.green[100]),
         ),
         child : Text(
-          style: textStyle(18, isInUse == true ? Colors.green[400] : Colors.green[200] , FontWeight.w500),
+          style: textStyle(18, widget.isInUse == true ? Colors.green[600] : Colors.green[200] , FontWeight.w500),
           "${widget.focusDuration.round()} | ${widget.pauseDuration.round()}",
         ),
         onPressed: () {
           provider.changeTimesDuration(widget.focusDuration, widget.pauseDuration, widget.focusDuration);
           provider.resetTimer();
-          isInUse = true;
+          // provider widget.isInUse = !widget.isInUse;
         },
       ),
     );
